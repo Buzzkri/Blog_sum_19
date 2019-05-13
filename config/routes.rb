44a69444282ed624e61d3a302b1f18c4ddfbfeba
Aaron Krishnan <buzzkri@gmail.com>
2019-05-13 14:44:19 -0700

    1.3 final commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
+
+  get "/about", to: "static_pages#about"
+  get "/home", to: "static_pages#home"
+
   resources :pages
+  # resources :static_pages
 end
